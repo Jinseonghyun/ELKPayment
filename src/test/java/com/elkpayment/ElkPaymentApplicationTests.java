6c0c9699925fbd223223dae873427170484c1aeb
Jinseonghyun <jinsunghyun9@gmail.com>
2024-09-30 10:51:13 +0900

    logstach -> filebeat 로 변경

--- src/test/java/com/elkpayment/ElkPaymentApplicationTests.java
+++ src/test/java/com/elkpayment/ElkPaymentApplicationTests.java
@@ -2,8 +2,13 @@ package com.elkpayment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.elasticsearch.host=localhost:9200",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration"
+})
 class ElkPaymentApplicationTests {
 
     @Test
